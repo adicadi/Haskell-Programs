9a8930d17ed95cf87e01066504b7b52a5a49a2dd
Aditya chaudhary <adicadi158@gmail.com>
2025-01-19 21:27:28 +0100

    ExerciseSheet-6

--- TaskSheets.hs
+++ TaskSheets.hs
@@ -1,7 +1,8 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use foldr" #-}
 {-# HLINT ignore "Use ++" #-}
-import Prelude hiding (last, (++), reverse, filter, init, concat, splitAt)
+{-# HLINT ignore "Use sum" #-}
+import Prelude hiding (Maybe(..), foldl, foldr, last, (++), reverse, filter, init, concat, splitAt)
 
 
 --Task-3: Functions in Haskell
@@ -63,6 +64,82 @@ splitAt n (x:xs) =
 
 
 
+
+
+
+--2) listprefix to identify the prefix of a list
+listprefix :: Int -> [a] -> [a]
+listprefix _ [] = [] --Base Case:Prefix of an empty list is an empty list. 
+listprefix n (x:xs) 
+    | n < 1 = [] --if n is less than 1, return empty list.
+    | otherwise = x : listprefix(n-1) xs --Recursive case: Prepend x and recursively call listprefix (n-1) xs to get the rest of the list.   
+
+
+
+--3) sublist: extract a sublist of length m starting at position n in the input list. 
+sublist :: Int -> Int -> [a] -> [a]
+sublist m n xs = take m (drop n xs) -- Drop 'n' elements from the beginning of the list xs and take the first m elements from the remaining list from drop. 
+
+
+--4) powlist: to generate the power set of a list. 
+powlist :: [a] -> [[a]]
+powlist [] = [[]] --Base Case: Power set of an empty list is a list containing an empty list. 
+powlist (x:xs) = let subsets = powlist xs --Compute the powerset of the smaller list.  
+                in subsets ++ map (x:) subsets --Take each subset of xs(stored in subsets) and prepend x to it. This creates new subsets that include x. 
+                --Recursive case: Append the subsets of the rest of the list to the subsets of the rest of the list with x prepended to each subset.
+{-
+    -When the input is not empty, it is split into, x: The first element of the list and xs: The rest of the list. 
+    -subsets(all subsets of xs without x)
+    -map (x:) subsets: (all subsets of xs with x prepended to each subset)
+-}
+
+
+
+
+--Task-4 
+
+
+--2) Implementing foldr and foldl using recursion.
+foldr :: (a->b->b) -> b -> [a] -> b -- A binary function(a->b->b), an initial value of type b and a list of type a, returning a value of type b.
+foldr _ acc [] = acc --Base Case: Folding an empty list returns the accumulator.
+foldr f acc (x:xs) = f x (foldr f acc xs)  --Recursive case: Apply the function f to the current element x and the result of folding the rest of the list.
+
+
+--Implementing foldl using recursion.
+foldl :: (b->a->b) -> b -> [a] ->b -- A binary function(b->a->b), an initial value of type b and a list of type a, returning a value of type b.
+foldl _ acc [] = acc --Base Case: Folding an empty list returns the accumulator. 
+foldl f acc (x:xs) = foldl f (f acc x) xs --Recursive case: Apply the function f to the accumulator and the current element x, then fold the rest of the list.
+
+
+--Implementing lastElement using foldl
+data Maybe a = Nothing | Just a
+
+lastElement :: [a] -> Maybe a
+lastElement = foldl (\_ x -> Just x) Nothing --Fold the list using foldl, with the accumulator being Nothing and the function that returns Just x for the current element x.
+
+
+
+
+--Implementing the none function using foldr
+none :: (a -> Bool) -> [a] ->Bool
+none p = foldr (\x acc -> not (p x) && acc)True --Fold the list using foldr, with the accumulator being True and the function that returns True if the predicate p is False for the current element x and the accumulator is True.
+
+
+
+--Implementing exist function using foldr
+exist :: (a -> Bool) -> [a] -> Bool
+exist p = foldr (\x acc -> p x || acc) False --Fold the list using foldr, with the accumulator being False and the function that returns True if the predicate p is True for the current element x and the accumulator is False.
+
+
+
+
+--Implementing exactlyOne using the foldl function
+exactlyOne :: (a ->Bool) -> [a] -> Bool
+exactlyOne p = (==1).foldl (\count x -> if p x then count +1 else count) 0 --Fold the list using foldl, with the accumulator being 0 and the function that increments the count if the predicate p is True for the current element x, then checks if the count is equal to 1.
+
+
+
+
 --Test Cases
 main :: IO ()
 main = do
@@ -73,3 +150,11 @@ main = do
   print $ init [1,2,3]
   print $ concat [[1,2],[3,4]]
   print $ splitAt 2 [1,2,3,4]
+  print $ listprefix 2 [1,2,3,4]
+  print $ sublist 2 4 [1,2,3,4,5,6]
+  print $ powlist [1,2,3,4]
+  print $ foldr (+) 0 [1,2,3,4]
+  print $ foldl (+) 0 [1,2,3,4]
+  print $ none even [1,3,5,2]
+  print $ exist even [1,3,5,2]
+  print $ exactlyOne even [1,3,5,2]
